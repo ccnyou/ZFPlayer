40fa0dcb76db860ea72933bdd9450cb933647a4b
chencongning <chencongning@bytedance.com>
2025-02-23 20:04:35 +0800

    * bugfix：处理无法横屏问题。

--- ZFPlayer/Classes/Core/ZFLandscapeViewController.m
+++ ZFPlayer/Classes/Core/ZFLandscapeViewController.m
@@ -46,7 +46,7 @@
 }
 
 - (UIInterfaceOrientationMask)supportedInterfaceOrientations {
-    return UIInterfaceOrientationMaskAllButUpsideDown;
+    return UIInterfaceOrientationMaskLandscapeRight;
 }
 
 - (BOOL)prefersHomeIndicatorAutoHidden {
